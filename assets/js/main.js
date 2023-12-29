fae76f2b87c785e561f5143b0b7c96cac2829719
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-12-29 12:45:35 +0100

    add script logic for nav menu and reveal animations

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*===== MENU SHOW LOGIC=====*/
+const toggleBars = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+if (toggleBars && navMenu) {
+	toggleBars.addEventListener('click', () => {
+		navMenu.classList.toggle('show');
+	})
+}
+
+/*===== REMOVE MENU ON MOBILE WHEN NAV ITEM CLICKED=====*/
+const navLinks = document.querySelectorAll('.nav__link');
+
+navLinks.forEach((navLink) => navLink.addEventListener('click', () => {
+	navMenu.classList.remove('show');
+}))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 400;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+	origin: 'top',
+	distance: '60px',
+	duration: 2000,
+	delay: 200,
+});
+
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
