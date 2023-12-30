00a4b4593a4381cfa5e115c13078ad5a701f92ea
Vladimir Davidov <vladimir.davidov.pro@gmail.com>
2023-12-30 17:30:08 +0100

    add script to send emails from forms via smtp js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,20 +18,20 @@ navLinks.forEach((navLink) => navLink.addEventListener('click', () => {
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+function scrollActive() {
+	const scrollY = window.pageYOffset
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 400;
-        sectionId = current.getAttribute('id')
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight
+		const sectionTop = current.offsetTop - 400;
+		sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+		} else {
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+		}
+	})
 }
 window.addEventListener('scroll', scrollActive)
 
@@ -44,12 +44,33 @@ const sr = ScrollReveal({
 	delay: 200,
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text', {});
+sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img', { delay: 400 });
+sr.reveal('.home__social-icon', { interval: 200 });
+sr.reveal('.skills__data, .work__img, .contact__input', { interval: 200 });
 
-/*========= SEND EMAIL FROM THE FORM ===========*/
-function sendEmail() {
+/*========= SEND EMAIL FROM THE FORM AND RETURN TO HOME SECTION ===========*/
+function emailSend() {
+	const form = document.getElementById("contact__form");
+	const data = new FormData(form)
 
+	Email.send({
+		Host: "smtp.elasticemail.com",
+		Username: "vdmailx@gmail.com",
+		Password: "0D6855CFA7B73D3E957B0664286FAEAE2B67",
+		To: 'vladimir.davidov.pro@gmail.com',
+		From: "vdmailx@gmail.com",
+		Subject: "Portfolio Website - New message",
+		Body: `There's a new message from a visitor of your portfolio website.<br>
+		His email is: ${data.get('email')}.<br>
+		His name is: ${data.get('name')}.<br>
+		His message is the following ${data.get('message')}`
+}).then(
+			message => alert(message)
+		);
 }
+
+document.getElementById("contact__form").addEventListener("submit", function (event) {
+	event.preventDefault();
+	this.reset();
+})
